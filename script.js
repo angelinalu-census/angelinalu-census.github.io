b793ad5bad745745d220738be39c3286640067e8
Angelina L Lu <angelina.l.lu@census.gov>
2021-02-10 14:30:17 -0500

    changed content to display EEOALL1R_310.csv

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-function myFunction() {
-    document.getElementById("demo").innerHTML="Paragraph changed.";
-}
+src="d3.min.js?v=3.2.8"
+        type="text/javascript";
+        charset="utf-8"
+            d3.text("T:\tab6\spectabs\prod\st109\2014thru2018_orig\ftp\EEO_Tables_By_Geographic_Area\Core_Based_Statistical_Areas\EEOALL1R_310.csv", function(data) {
+                var parsedCSV = d3.csv.parseRows(data);
+
+                var container = d3.select("body")
+                    .append("table")
+
+                    .selectAll("tr")
+                        .data(parsedCSV).enter()
+                        .append("tr")
+
+                    .selectAll("td")
+                        .data(function(d) { return d; }).enter()
+                        .append("td")
+                        .text(function(d) { return d; });
+            });

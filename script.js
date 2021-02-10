83af2145378df4a6aa53ced2e510d393a100a526
Angelina L Lu <angelina.l.lu@census.gov>
2021-02-10 14:51:48 -0500

    changed location of csv

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 src="d3.min.js?v=3.2.8"
         type="text/javascript";
         charset="utf-8"
-            d3.text("T:\tab6\spectabs\prod\st109\2014thru2018_orig\ftp\EEO_Tables_By_Geographic_Area\Core_Based_Statistical_Areas\EEOALL1R_310.csv", function(data) {
+            d3.text("EEOALL10W_C21.csv", function(data) {
                 var parsedCSV = d3.csv.parseRows(data);
 
                 var container = d3.select("body")

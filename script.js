c5a77aac019161f0c060b13a42fa581175fa4384
Angelina L Lu <angelina.l.lu@census.gov>
2021-02-10 17:28:12 -0500

    fixed the table not displaying

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-src="d3.min.js?v=3.2.8"
-        type="text/javascript";
-        charset="utf-8"
-            d3.text("EEOALL10W_C21.csv", function(data) {
-                var parsedCSV = d3.csv.parseRows(data);
+d3.text("EEOALL10W_C21.csv", function(datasetText) {
 
-                var container = d3.select("body")
-                    .append("table")
-
-                    .selectAll("tr")
-                        .data(parsedCSV).enter()
-                        .append("tr")
-
-                    .selectAll("td")
-                        .data(function(d) { return d; }).enter()
-                        .append("td")
-                        .text(function(d) { return d; });
-            });
+    var parsedCSV = d3.csv.parseRows(datasetText);
+    
+    var sampleHTML = d3.select("#viz")
+        .append("table")
+        .style("border-collapse", "collapse")
+        .style("border", "2px black solid")
+    
+        .selectAll("tr")
+        .data(parsedCSV)
+        .enter().append("tr")
+    
+        .selectAll("td")
+        .data(function(d){return d;})
+        .enter().append("td")
+        .style("border", "1px black solid")
+        .style("padding", "5px")
+        .text(function(d){return d;})
+        .style("font-size", "12px");
+    });
